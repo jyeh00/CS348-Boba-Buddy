17fb5ff2cbf25e7fec954e147a7237282eb196ed
Ariana Zhu <zhu811@purdue.edu>
2021-11-02 21:07:11 -0400

    Updating admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from . models import Drink
+from . models import Drink, Order, Temperature, Milk, Tea, Flavor, Topping
 
 # Register your models here.
 admin.site.register(Drink)
+admin.site.register(Order)
+admin.site.register(Temperature)
+admin.site.register(Milk)
+admin.site.register(Tea)
+admin.site.register(Flavor)
+admin.site.register(Topping)
