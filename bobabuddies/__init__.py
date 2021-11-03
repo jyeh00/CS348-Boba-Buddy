18141d90a64602cf5bb1514d01f8f56c28d500a7
David Li <li3306@purdue.edu>
2021-11-03 12:58:30 -0400

    changed string to r thingy in init

--- bobabuddies/__init__.py
+++ bobabuddies/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
 # David's
-sys.path.insert(0, "c:\users\david\projects\bobabuddy\cs348-boba-buddy\myproject\lib\site-package")
+sys.path.insert(0, r'c:\users\david\projects\bobabuddy\cs348-boba-buddy\myproject\lib\site-package')
 
 # Ariana's
 # sys.path.insert(0,"/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages")
