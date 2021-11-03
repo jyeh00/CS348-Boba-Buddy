a57afad9eb823d7de942aeaf080e57fe2a700db6
David Li <li3306@purdue.edu>
2021-11-03 12:52:38 -0400

    added sys path to init

--- bobabuddies/__init__.py
+++ bobabuddies/__init__.py
@@ -1,4 +1,9 @@
-# import sys
+import sys
+
+# David's
+sys.path.insert(0, "c:\users\david\projects\bobabuddy\cs348-boba-buddy\myproject\lib\site-package")
+
+# Ariana's
 # sys.path.insert(0,"/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages")
 import pymysql
 pymysql.version_info = (1, 4, 2, "final", 0)
