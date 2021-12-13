e61e788357548f32ea6070fa908c71f6b367ad10
Jessica Yeh <jessicayeh@Jessicas-Air.zyxel.com>
2021-12-12 22:53:50 -0500

    look up orders

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,8 @@ class OrderForm(ModelForm):
     class Meta:
         model = Drink
         exclude = ['drink_id', 'order', 'drink_flavor', 'drink_price', 'tea']
+
+class LookupForm(ModelForm):
+    class Meta:
+        model = Order
+        exclude = ['order_price']
