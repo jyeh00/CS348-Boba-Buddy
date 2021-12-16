9a0412cf06e7b9b8828e800efa580fe4d366a71e
David Li <li3306@purdue.edu>
2021-12-16 03:49:52 -0500

    database stuff

--- bobabuddies/settings.py
+++ bobabuddies/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,7 +94,7 @@ DATABASES = {
         'PASSWORD': 'BobaBuddy',
         'HOST': '34.123.254.147',
         'PORT': '3306',
-        'DISABLE_SERVER_SIDE_CURSORS': True,
+
     }
 }
 
